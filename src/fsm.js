19ec2fce742a86f95ce086d3e7caa67530519186
unknown <paprotski1702@gmail.com>
2017-10-01 12:19:23 +0300

    1

--- src/fsm.js
+++ src/fsm.js
@@ -3,19 +3,39 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config)
+    {
+        this.initial=config.initial;
+        this.states=config.states;
+        this.states.normal.transitions.study=config.states.normal.transitions.study;
+        this.states.busy.transitions.get_tired=config.states.busy.transitions.get_tired;
+        this.states.busy.transitions.get_hungry=config.states.busy.transitions.get_hungry;
+        this.states.hungry.transitions.eat=config.states.hungry.transitions.eat;
+        this.states.sleeping.transitions.get_hungry=config.states.sleeping.transitions.get_hungry;
+        this.states.sleeping.transitions.get_up=config.states.sleeping.transitions.get_up;
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState()
+    {
+        return this.initial;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state)
+    {
+        if(state=="hungry"||state=="busy"||state=="normal"||state=="sleeping")
+        {
+            this.initial=state;
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
